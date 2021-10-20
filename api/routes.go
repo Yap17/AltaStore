c7c5f2e5390ef215639c7a68d17d881ca09a3408
Yap <yapalex1994@gmail.com>
2021-10-20 13:33:33 +0700

    add insert update delete user

--- api/routes.go
+++ api/routes.go
@@ -2,15 +2,22 @@ package api
 
 import (
 	"AltaStore/api/v1/category"
+	"AltaStore/api/v1/user"
 
 	echo "github.com/labstack/echo/v4"
 )
 
-func RegisterPath(e *echo.Echo, category *category.Controller) {
-	if category == nil {
+func RegisterPath(e *echo.Echo, category *category.Controller, userController *user.Controller) {
+	if category == nil || userController == nil {
 		panic("Invalid parameter")
 	}
 
 	cat := e.Group("v1/categories")
 	cat.GET("", category.GetAllCategory)
+
+	user := e.Group("v1/users")
+	//userV1.GET("/:id", userController.FindUserByID)
+	user.POST("", userController.InsertUser)
+	user.PUT("/:id", userController.UpdateUser)
+	user.DELETE("/:id", userController.DeleteUser)
 }
