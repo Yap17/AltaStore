f33dfc8f679aa2ac4cae6906616da9069e2ee41a
Widya Ade Bagus <dewidyabagus@gmail.com>
2021-10-21 17:17:57 +0700

    Penambahan migrasi tabel shopping_carts dan shopping_cart_details

--- modules/migration/migration.go
+++ modules/migration/migration.go
@@ -2,11 +2,17 @@ package migration
 
 import (
 	"AltaStore/modules/category"
+	"AltaStore/modules/shopping"
 	"AltaStore/modules/user"
 
 	"gorm.io/gorm"
 )
 
 func TableMigration(db *gorm.DB) {
-	db.AutoMigrate(&category.ProductCategory{}, &user.User{})
+	db.AutoMigrate(
+		&category.ProductCategory{},
+		&user.User{},
+		&shopping.ShoppingCart{},
+		&shopping.ShoppingCartDetail{},
+	)
 }
