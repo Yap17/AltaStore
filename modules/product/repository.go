b6874f07f3950a7df051546b2289636c7a4383c2
Widya Ade Bagus <dewidyabagus@gmail.com>
2021-10-24 16:07:10 +0700

    Perubahan dari preload where orm menjadi raw query

--- modules/product/repository.go
+++ modules/product/repository.go
@@ -3,6 +3,7 @@ package product
 import (
 	"AltaStore/business/product"
 	category "AltaStore/modules/category"
+	"fmt"
 	"strconv"
 	"time"
 
@@ -29,6 +30,42 @@ type Product struct {
 	UpdatedBy         string    `gorm:"updated_by;type:varchar(50)"`
 	DeletedAt         time.Time `gorm:"deleted_at"`
 	DeletedBy         string    `gorm:"deleted_by;type:varchar(50)"`
+}
+
+type ProductQuery struct {
+	ID                  string    `gorm:"id"`
+	Code                string    `gorm:"code"`
+	Name                string    `gorm:"name"`
+	Price               int64     `gorm:"price"`
+	ProductCategoryName string    `gorm:"product_category_name"`
+	IsActive            bool      `gorm:"isactive"`
+	UnitName            string    `gorm:"unitname"`
+	Description         string    `gorm:"description"`
+	Qty                 int       `gorm:"qty"`
+	QtyCart             int       `gorm:"qty_cart"`
+	CreatedAt           time.Time `gorm:"created_at"`
+	CreatedBy           string    `gorm:"created_by"`
+	UpdatedAt           time.Time `gorm:"updated_at"`
+	UpdatedBy           string    `gorm:"updated_by"`
+}
+
+func (p *ProductQuery) ToProduct() product.Product {
+	return product.Product{
+		ID:                  p.ID,
+		Code:                p.Code,
+		Name:                p.Name,
+		Price:               p.Price,
+		IsActive:            p.IsActive,
+		UnitName:            p.UnitName,
+		Description:         p.Description,
+		ProductCategoryName: p.ProductCategoryName,
+		Qty:                 int32(p.Qty),
+		QtyCart:             int32(p.QtyCart),
+		CreatedAt:           p.CreatedAt,
+		CreatedBy:           p.CreatedBy,
+		UpdatedAt:           p.UpdatedAt,
+		UpdatedBy:           p.UpdatedBy,
+	}
 }
 
 func (p *Product) ToProduct() product.Product {
@@ -74,21 +111,26 @@ func NewRepository(db *gorm.DB) *Repository {
 }
 
 func (r *Repository) GetAllProduct() (*[]product.Product, error) {
-	var products []Product
-	var tempProduct product.Product
+	var products []ProductQuery
+	// var tempProduct product.Product
 
-	err := r.DB.Preload("ProductCategory").Where("deleted_by = ''").Find(&products).Error
+	// err := r.DB.Preload("ProductCategory").Where("deleted_by = ''").Find(&products).Error
+	var query = "select t1.*, stock_akhir qty, stock_cart qty_cart, t2.product_category_name from products t1" +
+		" inner join product_stock(t1.id) on productid = t1.id " +
+		" inner join product_categories t2 on t2.id = product_category_id " +
+		" where deleted_by = ''"
+	err := r.DB.Raw(query).Scan(&products).Error
 	if err != nil {
 		return nil, err
 	}
 
 	var productOuts []product.Product
 	for _, value := range products {
-		tempProduct = value.ToProduct()
-		tempProduct.ProductCategoryName = value.ProductCategory.Name
-		tempProduct.Qty = 10
-		tempProduct.QtyCart = 0
-		productOuts = append(productOuts, tempProduct)
+		// tempProduct = value.ToProduct()
+		// tempProduct.ProductCategoryName = value.ProductCategory.Name
+		// tempProduct.Qty = 10
+		// tempProduct.QtyCart = 0
+		productOuts = append(productOuts, value.ToProduct()) //tempProduct)
 	}
 
 	return &productOuts, nil
@@ -96,45 +138,62 @@ func (r *Repository) GetAllProduct() (*[]product.Product, error) {
 
 func (r *Repository) GetAllProductByParameter(id, isActive, categoryName,
 	code, name string) (*[]product.Product, error) {
-	var products []Product
-	var tempProduct product.Product
+	// var products []Product
+	var products []ProductQuery
+	// var tempProduct product.Product
 	var productOuts []product.Product
 
-	temp := r.DB
-	temp = temp.Preload("ProductCategory")
+	var query = "select t1.*, stock_akhir qty, stock_cart qty_cart, t2.name product_category_name from products t1" +
+		" inner join product_stock(t1.id) on productid = t1.id " +
+		" inner join product_categories t2 on t2.id = product_category_id " +
+		" where t1.deleted_by = ''"
+
+	// temp := r.DB
+	// temp = temp.Preload("ProductCategory")
 	if categoryName != "" {
-		temp = temp.Joins("inner join product_categories on product_categories.id = products.product_category_id")
-		temp = temp.Where("product_categories.name = ?", categoryName)
+		// temp = temp.Joins("inner join product_categories on product_categories.id = products.product_category_id")
+		// temp = temp.Where("product_categories.name = ?", categoryName)
+		query += fmt.Sprintf(" and t2.name = '%s'", categoryName)
 	}
+
 	if id != "" {
-		temp = temp.Where("id = ?", id)
+		// temp = temp.Where("id = ?", id)
+		query += fmt.Sprintf(" and t1.id = '%s'", id)
 	}
 	if isActive != "" {
 		res, err := strconv.ParseBool(isActive)
 		if err != nil {
 			return &productOuts, nil
 		}
-		temp = temp.Where("is_active = ?", res)
+		// temp = temp.Where("is_active = ?", res)
+		query += fmt.Sprintf(" and t1.is_active = %t", res)
 	}
 	if code != "" {
-		temp = temp.Where("code = ?", code)
+		// temp = temp.Where("code = ?", code)
+		query += fmt.Sprintf(" and t1.code = '%s'", code)
 	}
 	if name != "" {
-		temp = temp.Where("name = ?", name)
+		// temp = temp.Where("name = ?", name)
+		query += fmt.Sprintf(" and t1.name = '%s'", name)
 	}
 
-	err := temp.Where("deleted_by = ''").Find(&products).Error
+	// err := temp.Where("deleted_by = ''").Find(&products).Error
+	err := r.DB.Raw(query).Scan(&products).Error
 	if err != nil {
 		return nil, err
 	}
 
-	for _, value := range products {
-		tempProduct = value.ToProduct()
+	// for _, value := range products {
+	// 	tempProduct = value.ToProduct()
 
-		tempProduct.ProductCategoryName = value.ProductCategory.Name
-		tempProduct.Qty = 10
-		tempProduct.QtyCart = 0
-		productOuts = append(productOuts, tempProduct)
+	// 	tempProduct.ProductCategoryName = value.ProductCategory.Name
+	// 	tempProduct.Qty = 10
+	// 	tempProduct.QtyCart = 0
+	// 	productOuts = append(productOuts, tempProduct)
+	// }
+
+	for _, value := range products {
+		productOuts = append(productOuts, value.ToProduct())
 	}
 
 	return &productOuts, nil
